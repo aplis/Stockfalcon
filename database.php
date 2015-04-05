67bdae2b3a4630e876d9267a6337b0e23aa62c8c
feeling13 <shilpiagarwal373@gmai.com>
2015-04-05 21:32:13 +0530

    Update database.php

--- database.php
+++ database.php
@@ -32,6 +32,7 @@
 			    $result = mysql_query($sql);
 			    return $result;
 			}
+			
 
 			function delete($table, $field, $input)
 			{
@@ -57,6 +58,71 @@
 
 				return $flag;
 			}
+			function update( $table , $email,$gender,$timestamp,$first_name,$last_name,$birthdate)
+			{
+				
+				$sql="UPDATE ".DB_NAME.".".$table." SET email='".$email."',gender='".$gender."',first_name='".$first_name."',last_name='".$last_name."',birthdate='".$birthdate."' where email='".$email."'";
+			        echo $sql;
+				$result = mysql_query($sql);
+			        return $result;
+			}
+			function changepassword( $table , $o_password,$n_password,$temp_password)
+			{
+				if($n_password==$temp_password)
+				$sql="UPDATE ".DB_NAME.".".$table." SET password='".$n_password."' where password='".$o_password."'";
+				$result = mysql_query($sql);
+			        return $result;
+			}
+			function polling( $table ,$stock_name)
+			{
+
+				if(isset($_POST['submit1'])){
+                               
+                                $sql="UPDATE ".DB_NAME.".".$table." SET buy_poll=buy_poll+1,total=buy_poll+sell_poll+hold_poll where stock_name='".$stock_name."'";
+                                $result = mysql_query($sql);
+				return $result;
+
+   				 }
+   			        elseif(isset($_POST['submit2']))
+                                 {
+                           	$sql="UPDATE ".DB_NAME.".".$table." SET sell_poll=sell_poll+1,total=buy_poll+sell_poll+hold_poll where  stock_name='".$stock_name."'";
+			        $result = mysql_query($sql);
+			    	return $result;
+                           }
+  								  
+  				elseif(isset($_POST['submit3']))
+                                 {
+                          	$sql="UPDATE ".DB_NAME.".".$table." SET hold_poll=hold_poll+1,total=buy_poll+sell_poll+hold_poll where  stock_name='".$stock_name."'";
+				$result = mysql_query($sql);
+			    	return $result;
+                                   }
+                          elseif(isset($_POST['submit4']))
+                          {
+                          	 $sql2= "SELECT * from  ".DB_NAME.".".$table." where stock_name='".$stock_name."'" ;
+                                 $result2 = mysql_query($sql2);
+                                 $user = mysql_fetch_assoc($result2);
+                              	if($user['total']!=0)
+				{
+					echo "The % of user who had polled buying the stock ";
+					echo ($user['buy_poll']/$user['total'])*100; 
+						
+    					}
+                          }
+              
+			}
+			function suggest( $table )
+
+			{
+				$sql2= "SELECT * from  ".DB_NAME.".".$table." order by predicted_value desc limit 10" ; 
+                                $result2 = mysql_query($sql2);
+                              
+                              while($row = mysql_fetch_array($result2, MYSQL_ASSOC) ) 
+					{
+					echo $row['stock_name'];
+        				echo " ";
+    				}
+                              return $result2;
+                        }
 
 			function hash_password($password, $nonce) 
 			{
